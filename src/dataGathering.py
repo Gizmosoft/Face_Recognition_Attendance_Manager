374487e88827898a6c9a41fd445a47a1bb19955a
Gizmosoft <Kartikey.hebbar@gmail.com>
2021-08-05 23:08:20 +0530

    working on the detector module

--- src/dataGathering.py
+++ src/dataGathering.py
@@ -6,48 +6,49 @@ import sys
 from datasetconfig import get_and_update_counter
 from json_operator import write_to_json_file
 
-# cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
+def data_gatherer():
+    # cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video width
+    cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier('../database/haarcascade_frontalface_default.xml')
+    face_detector = cv2.CascadeClassifier('../database/haarcascade_frontalface_default.xml')
 
-# For each person, enter one numeric face id - get the face id from get_counter()
-face_id = get_and_update_counter()
-username = input("Please enter the name of the person: ")
-# Store the user data into the json database
-datastorage_status = write_to_json_file(face_id, username)
-# Print and check if it ran to some error - this is a very bad way of checking the exception case but lol, who's gonna read my code!! :P
-if(datastorage_status == "There was some error storing the userdata!"):
-    sys.exit(datastorage_status)
-print(datastorage_status)
-print("\n [INFO] Initializing face capture. Hi " + str(username) + ", look in the camera and wait ...")
-time.sleep(2)
-# Initialize individual sampling face count
-count = 0
+    # For each person, enter one numeric face id - get the face id from get_counter()
+    face_id = get_and_update_counter()
+    username = input("Please enter the name of the person: ")
+    # Store the user data into the json database
+    datastorage_status = write_to_json_file(face_id, username)
+    # Print and check if it ran to some error - this is a very bad way of checking the exception case but lol, who's gonna read my code!! :P
+    if(datastorage_status == "There was some error storing the userdata!"):
+        sys.exit(datastorage_status)
+    print(datastorage_status)
+    print("\n [INFO] Initializing face capture. Hi " + str(username) + ", look in the camera and wait ...")
+    time.sleep(2)
+    # Initialize individual sampling face count
+    count = 0
 
-while(True):
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    while(True):
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-        count += 1
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+            count += 1
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("../dataset/" + str(username)+ '_' + str(face_id) + '_' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+            # Save the captured image into the datasets folder
+            cv2.imwrite("../dataset/" + str(username)+ '_' + str(face_id) + '_' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
-        cv2.imshow('image', img)
+            cv2.imshow('image', img)
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
-        break
-    elif count >= 50: # Take 30 face sample and stop video
-         break
+        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 50: # Take 30 face sample and stop video
+            break
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
-cv2.destroyAllWindows()
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
--- src/json_operator.py
+++ src/json_operator.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 def write_to_json_file(face_id, username):
     if __name__ == '__write_to_json_file__':
@@ -20,19 +21,41 @@ def write_to_json_file(face_id, username):
     except:
         return "There was some error storing the userdata!"
 
-def get_id():
-    if __name__ == '__get_id__':
-        get_id()
+def read_json_file():
+    if __name__ == '__read_json_file__':
+        read_json_file()
 
     # load json file
     jsonFile = open("../database/userdata.json", 'r')
     values = json.load(jsonFile)
     jsonFile.close()
+    return values
 
-    # store id into variable
-    id_value = values['users'][0]['id']
-    name_value = values['users'][0]['name']
-    return id_value + " " + name_value
+def get_number_of_ids():            # Currently not using this
+    if __name__ == '__get_number_of_ids__':
+        get_number_of_ids()
 
-print(get_id())
+    values = read_json_file()
+    id_count = 0
+
+    for i in values['users']:
+        id_count = id_count + 1
+    return id_count
+
+def populate_name_array(name_value):
+    if __name__ == '__populate_name_array__':
+        populate_name_array(name_value)
+
+    # if(len(name_value)<2):
+    #     print("Insufficient User Database! Add more users to perform Face Recognition.")
+    #     sys.exit(name_value)
+
+    values = read_json_file()
+
+    #parse through the userdata json file
+    for i in values['users']:
+        name_value.append(i['name'])
+    return name_value
+
+
 # print(write_to_json_file(1,"Mason"))
